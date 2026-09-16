d4f4ae1952e4cfa4a0830031ade2cf1584bac14f
debugbastos <bastos.program@gmail.com>
2026-09-16 17:07:26 -0300

    ;rgb:bfbf

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import NavBar from "./componets/NavBar"
+import Hero from "./componets/Hero"
+import Intro from "./componets/Intro"
+import Highlights from "./componets/Highlights"
+import Colors from "./componets/Colors"
+import Footer from "./componets/footer"
+
+function App() {
+
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+      <Intro />
+      <Highlights />
+      <Colors />
+      <Footer />
+    </div>
+  )
+}
+ 
+export default App
